4b15f80a4516c822a1548edcc0ff6ebffe606226
imdark <mikesgoacademy@gmail.com>
2018-11-04 10:58:48 -0800

    Create linux.sh

---
+++ linux.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+
+# setup vi as default editor
+sudo update-alternatives --config editor
+
+# install xfce
+sudo apt-get install xubuntu-desktop
+
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+sudo apt-get install vim python-pip tmux git vlc vlc-nox speedtest-cli htop nodejs nvidia-384 bwm-ng screen
+
+# Disable pinch to zoom:
+xfconf-query --set false --channel xfwm4 --property /general/zoom_desktop
+
+sudo curl -fsSL get.docker.com | sh
+sudo usermod -aG docker development
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker login
+
+# Download sublime text https://www.sublimetext.com/3
